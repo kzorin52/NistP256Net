77b2ee8f774dc1b1b7a26d994e9eacb78dc01cd7
Temnij <kzorin52@gmail.com>
2024-02-03 15:10:17 +0600

    Добавьте файлы проекта.

---
+++ NistP256.cs
@@ -0,0 +1,67 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace NistP256Net;
+
+public static partial class NistP256
+{ 
+    private const string LibraryName = "NistP256";
+
+    static NistP256()
+    {
+        NativeLibrary.SetDllImportResolver(Assembly.GetExecutingAssembly(), LoadLibrary);
+    }
+    private static nint LoadLibrary(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
+    {
+        string platform;
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            libraryName = $"lib{libraryName}.so";
+            platform = "linux";
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            libraryName = $"{libraryName}.dll";
+            platform = "win";
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            libraryName = $"lib{libraryName}.dylib";
+            platform = "osx";
+        }
+        else
+            throw new PlatformNotSupportedException();
+
+        if (NativeLibrary.TryLoad(libraryName, assembly, searchPath, out var handle))
+            return handle;
+
+        var arch = RuntimeInformation.ProcessArchitecture.ToString().ToLowerInvariant();
+
+        return NativeLibrary.Load(Path.Combine("runtimes", $"{platform}-{arch}", "native", libraryName), assembly, searchPath);
+    }
+
+#pragma warning disable CA1401
+
+    [LibraryImport(LibraryName)]
+    public static unsafe partial void private_to_public_key(byte* secretKey, byte* publicKey);
+
+#pragma warning restore CA1401
+
+    /// <summary>
+    /// Get compressed NistP256 public key
+    /// </summary>
+    /// <param name="privateKey">private key</param>
+    /// <returns>compressed public key</returns>
+    public static unsafe byte[] GetPublicKey(ReadOnlySpan<byte> privateKey)
+    {
+        Span<byte> publicKey = stackalloc byte[33];
+
+        fixed (byte* pubKeyPtr = &MemoryMarshal.GetReference(publicKey), prvKeyPtr = &MemoryMarshal.GetReference(privateKey))
+        {
+            private_to_public_key(prvKeyPtr, pubKeyPtr);
+        }
+
+        return publicKey.ToArray();
+    }
+}
---
+++ NistP256Net.Tests/NistP256Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NistP256Net;
+
+namespace NistP256Net.Tests;
+
+[TestClass]
+[TestSubject(typeof(NistP256))]
+public class NistP256Test
+{
+    private static readonly ReadOnlyMemory<byte> PrivateKey = new(Convert.FromHexString("ac5b6284de78a208ebbb0f23a3438f2fab98331b43682235e3290ccca023e2a8"));
+    private static readonly byte[] PublicKey = Convert.FromHexString("03e263598d507e1de7bdcfb5d560497fdc2a77948e1955ba70db4a24d2554fab5e");
+
+    [TestMethod]
+    public void TestPrvToPub()
+    {
+        Assert.IsTrue(PublicKey.SequenceEqual(NistP256.GetPublicKey(PrivateKey.Span)));
+    }
+}
